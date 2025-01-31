db3ff559c0a41afe1ccb35d4b58283ab09d63d1f
Leandro Correa de Souza Almeida <leandro_csa@yahoo.com.br>
2025-01-31 01:54:11 -0300

    fix pagination

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -11,7 +11,7 @@ const Pagination: React.FC<PaginationProps> = ({ currentPage, totalPages, setCur
 
   const maxPagesToShow = 2; // Máximo de páginas visíveis antes de usar "..."
   let startPage = Math.max(1, currentPage - Math.floor(maxPagesToShow / 2));
-  let endPage = Math.min(totalPages, startPage + maxPagesToShow - 1);
+  const endPage = Math.min(totalPages, startPage + maxPagesToShow - 1);
 
   if (endPage - startPage + 1 < maxPagesToShow) {
     startPage = Math.max(1, endPage - maxPagesToShow + 1);
